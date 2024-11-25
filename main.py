6590e7e4fcd2ae6730f5c620641264a03c84a604
BossAHPGaming <aaravhpatel210@gmail.com>
2024-11-25 18:49:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+x = input("Enter value of X : ")
+y = input("Enter value of Y : ")
+z = input("Enter value of Z : ")
+
+temp = x
+x = y 
+y = z
+z = temp
+
+print("value of x after swapping", x)
+print("value of y after swapping", y)
+print("value of z after swapping", z)
